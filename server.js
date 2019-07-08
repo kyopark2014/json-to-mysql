827ad71f403333e966221c2aa3af29c0ea4f3522
John Park <e2npark@gmail.com>
2019-07-08 22:44:52 +0900

    initiate the database using mysql script

--- server.js
+++ server.js
@@ -85,67 +85,6 @@ app.get('/getdata', (req, res) => {
     })
 })
 
-// Create JJ table - which is required for Json to MySQL
-app.get('/createDatatable', (req, res) => {
-    req.getConnection( (error, conn) => {
-        if(error) {
-            console.log('Connection Failure...')
-            throw error
-        }    
-        let sql = 'CREATE TABLE IF NOT EXISTS my_db.mysql_data(seq INT AUTO_INCREMENT, time BIGINT, value FLOAT(3.3), PRIMARY KEY(seq))'
-        conn.query(sql, (err, result) => {
-            if(err) {
-                res.status(500).send(err.message)
-                throw err
-            }
-            console.log(result)
-
-            res.status(200).send('The table was created.')
-        })
-    })
-})
-
-// Create JJ table - which is required for Json to MySQL
-app.get('/createJJtable', (req, res) => {
-    req.getConnection( (error, conn) => {
-        if(error) {
-            console.log('Connection Failure...')
-            throw error
-        }    
-        let sql = 'CREATE TABLE IF NOT EXISTS my_db.jj(seq INT AUTO_INCREMENT,num INT, PRIMARY KEY(seq))'
-        conn.query(sql, (err, result) => {
-            if(err) {
-                res.status(500).send(err.message)
-                throw err
-            }
-            console.log(result)
-
-            res.send('The table was created.')
-        })
-    })
-})
-
-// Initialize JJ
-app.get('/initializejj', (req, res) => {
-    req.getConnection( (error, conn) => {
-        if(error) {
-            console.log('Connection Failure...')
-            throw error
-        }   
-
-        let sql = 'INSERT INTO my_db.jj (num) VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20),(21),(22),(23),(24),(25),(26),(27),(28),(29),(30),(31),(32),(33),(34),(35),(36),(37),(38),(39),(40),(41),(42),(43),(44),(45),(46),(47),(48),(49),(50),(51),(52),(53),(54),(55),(56),(57),(58),(59),(60),(61),(62),(63),(64),(65),(66),(67),(68),(69),(70),(71),(72),(73),(74),(75),(76),(77),(78),(79),(80),(81),(82),(83),(84),(85),(86),(87),(88),(89),(90),(91),(92),(93),(94),(95),(96),(97),(98),(99)'
-
-        console.log(sql);
-        conn.query(sql, (err,result) => {
-            if(err) throw err
-
-            console.log(result)
-            
-            res.send('Table JJ is intialized')
-        })
-    })
-})
-
 app.listen('8080',() => {
     console.log('Server started on port 8080')
 })
